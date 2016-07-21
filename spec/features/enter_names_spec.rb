6cf90f511d4e25fba711ad9765cc8b31ab47d493
liskowsky <karollebiedzinski@gmail.com>
2016-07-21 11:19:15 +0100

    21 adds web helpers file

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature "Battle game", :type => :feature do
   scenario "sets the names of the players" do
-    visit "/"
-    fill_in "player_1_name", :with => "Karol"
-    fill_in "player_2_name", :with => "Titus"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content ("Karol vs. Titus")
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "View hit points" do
   scenario "displays the hitpoints for Player 2" do
-    visit('/')
-    fill_in :player_1_name, with: 'Karol'
-    fill_in :player_2_name, with: 'Titus'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content("Titus: 10HP" )
   end
 end
