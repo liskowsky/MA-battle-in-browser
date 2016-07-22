8d4cdfa89e951a5c182a962a329ac4fa33e1d9f7
Richard Bates <richo225@gmail.com>
2016-07-22 10:39:27 +0100

    Ex26 Make controller skinny with

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,14 +2,21 @@ require "game"
 require "player"
 
 describe Game do
-  subject(:game){Game.new}
-  subject(:player_1){Player.new("Bob")}
-  subject(:player_2){Player.new("Bill")}
+  subject(:game){Game.new(player_1, player_2)}
+  subject(:player_1){double :player}
+  subject(:player_2){double :player}
   describe '#attack' do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
-      #WHY?????????????????????????????????????????     
+    end
+  end
+  describe "#initialize" do
+    it "accepts player_1" do
+      expect(game.player_1).to eq player_1
+    end
+    it "accepts player_2" do
+      expect(game.player_2).to eq player_2
     end
   end
 end
